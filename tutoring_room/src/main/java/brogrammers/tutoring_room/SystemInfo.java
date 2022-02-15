fa0a2527168c352f1b712cccf9057e782042312d
Kenyaholland <46533825+Kenyaholland@users.noreply.github.com>
2022-02-14 18:30:46 -0600

    Project set-up

---
+++ tutoring_room/src/main/java/brogrammers/tutoring_room/SystemInfo.java
@@ -0,0 +1,13 @@
+package brogrammers.tutoring_room;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ tutoring_room/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module brogrammers.tutoring_room {
+    requires javafx.controls;
+    exports brogrammers.tutoring_room;
+}
